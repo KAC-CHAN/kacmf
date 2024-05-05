761ff3ffe54438d3b1d5ccdc5ffdfe95a6518d39
KAC CHAN <kacchan@outlook.in>
2024-05-05 15:09:44 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -14,7 +14,8 @@ async def remove_all_subscribers(client, message):
     if message.from_user.id == int(owner_id):
         try:
             # Get the list of all members in the channel
-            async for member in client.iter_chat_members(channel_id):
+            members = await client.get_chat_members(channel_id)
+            for member in members:
                 # Restrict the member's permissions
                 await client.restrict_chat_member(
                     chat_id=channel_id,
